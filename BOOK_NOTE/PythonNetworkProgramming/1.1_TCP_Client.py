74922d639b34c1f27bc629800752c37fbd1e29d8
panxuelei <317118173pxl@gmail.com>
2019-10-31 19:40:54 +0800

    update

--- BOOK_NOTE/PythonNetworkProgramming/1.1_TCP_Client.py
+++
@@ -1,9 +0,0 @@
-from socket import *
-s = socket(AF_INET, SOCK_STREAM)
-s.connect(("www.python.org", 80))   # connect
-message = "GET /index.html HTTP/1.1\n\n"
-message = message.encode()
-s.send(message)  # send request
-data = s.recv(10000).decode()  # get response
-print(data)
-s.close()
--- BOOK_NOTE/PythonNetworkProgramming/PythonNetwork/Solutions/4_4/webserve.py
+++
@@ -1,9 +0,0 @@
-# webserve.py
-# Note: The program must be placed in the PythonClass/ folder
-
-from BaseHTTPServer import HTTPServer
-from CGIHTTPServer import CGIHTTPRequestHandler
-import os
-os.chdir("NetExercises")
-serv = HTTPServer(("",8080),CGIHTTPRequestHandler)
-serv.serve_forever()
